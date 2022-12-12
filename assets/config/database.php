bb01ba0e037f5474255baaf70a879f142d234196
Shakirs Franco <kirsshacofran@gmail.com>
2022-12-12 16:10:10 -0500

    d

--- assets/config/database.php
+++ assets/config/database.php
@@ -4,7 +4,7 @@ class Database {
     private $host = 'localhost';
     private $db_name = 'fanlupaz';
     private $username = 'root';
-    private $password = 'test';
+    private $password = '';
     private $charset = 'utf8mb4';
 
     public function connect() {
